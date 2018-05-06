6c5903d7d89fac27c3b562ca4c4103e8cda196fc
CrowGuy <solewolf580580@gmail.com>
2018-05-06 18:47:56 +0800

    Add files via upload

--- MasterPage/scripts/add_aside.js
+++ MasterPage/scripts/add_aside.js
@@ -10,10 +10,12 @@ function add_aside (url, archives) {
 			if ( url ) {
 				
 				var aside_contents = read_contents ( url + "MasterPage/aside_contents.txt" );
+				aside_contents = aside_contents.replace('{{relative_path}}', url);
 			}
 			else {
 				
 				var aside_contents = read_contents ( "MasterPage/aside_contents.txt" );
+				aside_contents = aside_contents.replace('{{relative_path}}', '');
 			}
 
 			if ( aside_contents ) {      
