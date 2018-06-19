9c216b7e3e63fbcf0bcf7cfa0fd5007bca31be5c
CrowGuy <solewolf580580@gmail.com>
2018-06-19 21:19:19 +0800

    Modify the code style format.

--- MasterPage/scripts/add_contents.js
+++ MasterPage/scripts/add_contents.js
@@ -11,24 +11,24 @@ function addHeader() {
   if (document.getElementById) {
     let header = document.getElementById('header');
     if (header) {
-	  let pathPrefix = relativePath();
-	  let navState = navSelected();
-	  let headerContents = readContents(pathPrefix + contentsSrc);
-	  if (headerContents) {
-	    headerContents = headerContents.replace('{{indexPrefix}}'
+      let pathPrefix = relativePath();
+      let navState = navSelected();
+      let headerContents = readContents(pathPrefix + contentsSrc);
+      if (headerContents) {
+	headerContents = headerContents.replace('{{indexPrefix}}'
 		                                        , pathPrefix);
         headerContents = headerContents.replace('{{aboutPrefix}}'
 		                                        , pathPrefix);
         headerContents = headerContents.replace('{{archivesPrefix}}'
 		                                        , pathPrefix);
-		headerContents = headerContents.replace('{{indexNav}}'
+	headerContents = headerContents.replace('{{indexNav}}'
 		                                        , navState.index);
         headerContents = headerContents.replace('{{aboutNav}}'
 		                                        , navState.about);
         headerContents = headerContents.replace('{{archivesNav}}'
 		                                        , navState.archives);					
         placeInOuterHtml(header, headerContents);
-	  }
+      }
     }
   }  
 }
@@ -41,8 +41,8 @@ function addFooter() {
   if (document.getElementById) {
     let footer = document.getElementById('footer');
     if (footer) {
-	  let pathPrefix = relativePath();
-	  let footerContents = readContents(pathPrefix + contentsSrc);
+      let pathPrefix = relativePath();
+      let footerContents = readContents(pathPrefix + contentsSrc);
       if (footerContents) {      
         placeInOuterHtml(footer, footerContents);
       }
@@ -58,8 +58,8 @@ function addAside() {
   if (document.getElementById) {
     let aside = document.getElementById('aside');
     if (aside) {
-	  let pathPrefix = relativePath();
-	  let asideContents = readContents(pathPrefix + contentsSrc);
+      let pathPrefix = relativePath();
+      let asideContents = readContents(pathPrefix + contentsSrc);
       if (asideContents) {
         asideContents = asideContents.replace('{{imageUrlPrefix}}'
 		                                      , pathPrefix);	  
@@ -78,8 +78,8 @@ function addArchives(htmlText) {
   if (document.getElementById) {
     let archives = document.getElementById('archives');
     if (archives) {
-	  let pathPrefix = relativePath();
-	  let archivesContents = readContents(pathPrefix + contentsSrc);
+      let pathPrefix = relativePath();
+      let archivesContents = readContents(pathPrefix + contentsSrc);
       if (archivesContents) {  
         archivesContents = archivesContents.replace('{{archives}}', htmlText);
         placeInOuterHtml(archives, archivesContents);
@@ -96,16 +96,16 @@ function addGroupArchives() {
   const PATH_PREFIX = relativePath();
   let xmlhttp = new XMLHttpRequest();
   xmlhttp.onreadystatechange = function() {
-	if (this.readyState == 4 && this.status == 200) {
-	  let groupList = JSON.parse(this.responseText);
-	  let htmlContents = '';
-	  for (let i = 0; i < groupList.length; i++) {
-		htmlContents += '<li><a href="' + PATH_PREFIX + 'type_group.html?type='
-		                + groupList[i].type_group + '">' 
-						+ groupList[i].type_group + '</a></li>\n';	
-	  }
-	  addArchives(htmlContents);
-	}
+    if (this.readyState == 4 && this.status == 200) {
+      let groupList = JSON.parse(this.responseText);
+      let htmlContents = '';
+      for (let i = 0; i < groupList.length; i++) {
+        htmlContents += '<li><a href="' + PATH_PREFIX + 'type_group.html?type='
+		     + groupList[i].type_group + '">' 
+                     + groupList[i].type_group + '</a></li>\n';	
+      }
+      addArchives(htmlContents);
+    }
   };
   xmlhttp.open("GET", PATH_PREFIX + JSON_SRC, true);
   xmlhttp.send();
@@ -129,7 +129,7 @@ function addNews(url, title, img, index) {
 	                                      ,'');
     }
     newsContents = newsContents.replace('{{acticleUrl}}', PATH_PREFIX + url);
-	newsContents = newsContents.replace('{{articleTitle}}', title);
+    newsContents = newsContents.replace('{{articleTitle}}', title);
     newsContents = newsContents.replace('{{articleImg}}', PATH_PREFIX + img);
     return newsContents + '\n';
   }  
@@ -144,24 +144,24 @@ function addLastArticles() {
   const NUM_LAST = 2;
   let xmlhttp = new XMLHttpRequest();	
   xmlhttp.onreadystatechange = function() {
-	if (this.readyState == 4 && this.status == 200) {
-	  let articleList = JSON.parse(this.responseText);
-	  articleList.reverse();
-	  let htmlContents = '';
-	  for (let i = 0; i < NUM_LAST; i++) {
-		let title = articleList[i].articles;
-		let img = articleList[i].img;
-		let url = articleList[i].url;
+    if (this.readyState == 4 && this.status == 200) {
+      let articleList = JSON.parse(this.responseText);
+      articleList.reverse();
+      let htmlContents = '';
+      for (let i = 0; i < NUM_LAST; i++) {
+        let title = articleList[i].articles;
+        let img = articleList[i].img;
+        let url = articleList[i].url;
         htmlContents += addNews(url, title, img, i);
-	  }
+      }
 	  
       if (document.getElementById) {
-		let news = document.getElementById('news');
+        let news = document.getElementById('news');
         if (news) {
-		  placeInOuterHtml(news, htmlContents);
-		}
-	  }
-	}
+          placeInOuterHtml(news, htmlContents);
+        }
+      }
+    }
   };
   xmlhttp.open("GET", PATH_PREFIX + JSON_SRC, true);
   xmlhttp.send();
@@ -207,7 +207,7 @@ function addPage(url, pre, next, total, groupSize) {
   if (pageContents) {
     let totalPage = countTotalPages(total, groupSize);
     let pageButtonState = settingPageButton(url, pre, next, totalPage);
-	pageContents = pageContents.replace('{{disabled-pre}}'
+    pageContents = pageContents.replace('{{disabled-pre}}'
 	                                    , pageButtonState.preShow);
     pageContents = pageContents.replace('{{disabled-next}}'
 	                                    , pageButtonState.nextShow);
@@ -231,37 +231,37 @@ function addArticlePosts() {
   let current = currentPage();
   xmlhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
-	  let articleList = JSON.parse(this.responseText);
-	  articleList.reverse();
-	  let len = articleList.length;
-	  let next = current + 1;
-	  let htmlContents = '';
-	  for (let i = 0; i < len; i++) {
-		if (i == GROUP_SIZE * next) {
-		  break;
-		}
-	    else {
-		  if (parseInt(i/GROUP_SIZE) == current) {
-		    let title = articleList[i].articles;
-		    let url = articleList[i].url;
-		    let date = articleList[i].date;
-		    let typeGroup = articleList[i].type_group;
-		    let abstracts = articleList[i].abstracts;
-		    let img = articleList[i].img;
-		    htmlContents += addBlogPost(title, url, date, typeGroup
+      let articleList = JSON.parse(this.responseText);
+      articleList.reverse();
+      let len = articleList.length;
+      let next = current + 1;
+      let htmlContents = '';
+      for (let i = 0; i < len; i++) {
+        if (i == GROUP_SIZE * next) {
+          break;
+        }
+	else {
+          if (parseInt(i/GROUP_SIZE) == current) {
+            let title = articleList[i].articles;
+            let url = articleList[i].url;
+            let date = articleList[i].date;
+            let typeGroup = articleList[i].type_group;
+            let abstracts = articleList[i].abstracts;
+            let img = articleList[i].img;
+            htmlContents += addBlogPost(title, url, date, typeGroup
 			                            , abstracts, img);
-		  } 
-		}
-	  }
-	  htmlContents += addPage('index.html?page=',current-1,next,len,GROUP_SIZE);
+          } 
+	}
+      }
+      htmlContents += addPage('index.html?page=',current-1,next,len,GROUP_SIZE);
 			
       if (document.getElementById) {
-		let articlePost = document.getElementById ('article_post');
+        let articlePost = document.getElementById ('article_post');
         if (articlePost) {
-		  placeInOuterHtml (articlePost, htmlContents);
-		}
-	  }
+          placeInOuterHtml (articlePost, htmlContents);
 	}
+      }
+    }
   };
   xmlhttp.open("GET", JSON_SRC, true);
   xmlhttp.send();
@@ -297,42 +297,42 @@ function addGroupArticles() {
   let current = currentPage();
   let type = typeOfGroup();
   xmlhttp.onreadystatechange = function() {
-	if (this.readyState == 4 && this.status == 200) {
-	  let articleList = JSON.parse(this.responseText);
-	  articleList.reverse();
-	  let len = articleList.length;
-	  let next = current + 1;
-	  let htmlContents = '<h2 class="type_title text-deeper">' + type 
+    if (this.readyState == 4 && this.status == 200) {
+      let articleList = JSON.parse(this.responseText);
+      articleList.reverse();
+      let len = articleList.length;
+      let next = current + 1;
+      let htmlContents = '<h2 class="type_title text-deeper">' + type 
 	                     + '</h2>\n<hr />\n';
-	  let groupArticles = [];
-	  for (let i = 0; i < len; i++) {
-		if (articleList[i].type_group == type) {
-		  groupArticles.push(articleList[i]);
-		}
-	  }
-	  let totalArticles = groupArticles.length;
-	  for (let j = 0; j < totalArticles; j++) {
-	    if (j == GROUP_SIZE * next) {
-		  break;
-	    }
-	    else {
-		  if (parseInt(j/GROUP_SIZE) == current) {
-		    let title = groupArticles[j].articles;
-		    let url = groupArticles[j].url;
-		    let abstracts = groupArticles[j].abstracts;
-		    let img = groupArticles[j].img;
-		    htmlContents += addTypePost(title, url, abstracts, img);
-		  } 
-	    }
-	  }
-	  htmlContents += addPage('type_group.html?type=' + type + '?page='
+      let groupArticles = [];
+      for (let i = 0; i < len; i++) {
+        if (articleList[i].type_group == type) {
+          groupArticles.push(articleList[i]);
+        }
+      }
+      let totalArticles = groupArticles.length;
+      for (let j = 0; j < totalArticles; j++) {
+        if (j == GROUP_SIZE * next) {
+          break;
+        }
+	else {
+          if (parseInt(j/GROUP_SIZE) == current) {
+            let title = groupArticles[j].articles;
+            let url = groupArticles[j].url;
+            let abstracts = groupArticles[j].abstracts;
+            let img = groupArticles[j].img;
+            htmlContents += addTypePost(title, url, abstracts, img);
+          } 
+	}
+      }
+      htmlContents += addPage('type_group.html?type=' + type + '?page='
 	                          , current-1, next, totalArticles, GROUP_SIZE);
-	  if (document.getElementById) {
-	    let groupType = document.getElementById('type_group');
+      if (document.getElementById) {
+        let groupType = document.getElementById('type_group');
         if (groupType) {
-		  placeInOuterHtml(groupType, htmlContents);
-	    }
-	  }
+          placeInOuterHtml(groupType, htmlContents);
+	}
+      }
     }
   };
   xmlhttp.open("GET", JSON_SRC, true);
@@ -346,23 +346,23 @@ function listArticles() {
   const JSON_SRC = 'MasterPage/json/article_list.json';
   let xmlhttp = new XMLHttpRequest();
   xmlhttp.onreadystatechange = function() {
-	if (this.readyState == 4 && this.status == 200) {
-	  let articleList = JSON.parse(this.responseText);
-	  articleList.reverse();
-	  let len = articleList.length;
-	  let htmlContents = "";
-	  for (let i = 0; i < len; i++) {
-		htmlContents += '<p><a href="' + articleList[i].url + '">' 
-		                + articleList[i].articles + '</a>&emsp;' 
-						+ articleList[i].date + '</p>\n';
-	  }
+    if (this.readyState == 4 && this.status == 200) {
+      let articleList = JSON.parse(this.responseText);
+      articleList.reverse();
+      let len = articleList.length;
+      let htmlContents = "";
+      for (let i = 0; i < len; i++) {
+        htmlContents += '<p><a href="' + articleList[i].url + '">' 
+		     + articleList[i].articles + '</a>&emsp;' 
+                     + articleList[i].date + '</p>\n';
+      }
 			
-	  if (document.getElementById) {
-		let archivesList = document.getElementById('archives_list');
-		if (archivesList) {
-		  placeInOuterHtml(archivesList, htmlContents);
-		}
-	  }
+      if (document.getElementById) {
+        let archivesList = document.getElementById('archives_list');
+        if (archivesList) {
+          placeInOuterHtml(archivesList, htmlContents);
+        }
+      }
     }
   };
   xmlhttp.open("GET", JSON_SRC, true);
